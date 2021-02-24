1abdefc0d936b3b8bc8296764bb83812f05fd1b9
Evan Miller <emiller.arkko@gmail.com>
2021-02-24 20:57:51 +0200

    Implement song parser

--- src/lyric_master/song_parser.clj
+++ src/lyric_master/song_parser.clj
@@ -1,14 +1,51 @@
-(ns lyric-master.songparser
+(ns lyric-master.song-parser
   (require [clojure.string :as str]))
 
-(defn- extract-bars [song])
+(def new-song-regex #"NEW SONG")
+(def title-line-regex #"TITLE.*[\n]")
+(def round-bracket-regex #"\(.*\)")
+(def square-bracket-regex #"\[.*\]")
+(def quoted-text-regex #"\".*\"")
+(def plain-text-with-spaces-regex #"[a-z|A-Z|\s]+")
 
-(defn- extract-words [song])
+(defn- remove-from-str [regex str]
+  (str/replace str regex ""))
 
-(defn parse-song-text [s]
-  (let [withot-brackets (str/replace s #"(/d+)" )]))
+(defn- find-artist [song]
+  (->> song
+       (re-find square-bracket-regex)
+       (re-find plain-text-with-spaces-regex)))
 
-(defn analyze-song [song-raw]
-  (let [song (parse-song-text song-raw)
-        bars (extract-bars song)
-        words (extract-words song)]))
+(defn- find-title [song]
+  (->> song
+       (re-find title-line-regex)
+       (re-find quoted-text-regex)
+       (re-find plain-text-with-spaces-regex)))
+
+(defn parse-song [s]
+  (let [remove-round-brackets (partial remove-from-str round-bracket-regex)
+        remove-square-brackets (partial remove-from-str square-bracket-regex)
+        remove-title-line (partial remove-from-str title-line-regex)
+
+        title (find-title s)
+        artist (find-artist s)
+        lyrics (-> s
+                   str/trim
+                   remove-round-brackets
+                   remove-square-brackets
+                   remove-title-line
+                   str/trim)]
+    {:title title
+     :artist artist
+     :lyrics lyrics}))
+
+(defn- extract-song-strings [song-file-str]
+  (let [raw-songs (str/split song-file-str new-song-regex)
+        ham-songs (remove str/blank? raw-songs)]
+    ham-songs))
+
+(defn parse-song-file-str [song-file-str]
+  (->> song-file-str
+      extract-song-strings
+      (map parse-song)))
+
