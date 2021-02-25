30647c1fa7e7bc50f51979a4aaee1a65af27cd4d
Evan Miller <emiller.arkko@gmail.com>
2021-02-25 17:12:47 +0200

    Add metrics to song analysis

--- src/lyric_master/song_analyzer.clj
+++ src/lyric_master/song_analyzer.clj
@@ -1,4 +1,5 @@
-(ns lyric-master.song-analyzer)
+(ns lyric-master.song-analyzer
+  (:require [lyric-master.rhyme-detection :as rhyme]))
 
 (defn word-used-times [words word]
   (->> words
@@ -6,11 +7,22 @@
        count))
 
 (defn analyze-song-lyrics [{:keys [words bars]}]
-  (let [words-sorted (sort words)
-        words-distinct (distinct words-sorted)]
-    {:bar-count (count bars)
-     :word-count (count words)
-     :distinct-word-count (count words-distinct)
+  (let [sorted-words (sort words)
+        distinct-words (distinct sorted-words)
+        rhymes (rhyme/filter-words-that-rhyme words)
+        rhyme-count (count rhymes)
+        distinct-rhymes (distinct rhymes)
+        distinct-rhyme-count (count distinct-rhymes)
+        bar-count (count bars)
+        word-count (count words)]
+    {:bar-count bar-count
+     :word-count word-count
+     :rhyme-count (count rhymes)
+     :distinct-word-count (count distinct-words)
+     :distinct-rhyme-count (count distinct-rhymes)
+     :distinct-rhyming-words distinct-rhymes
+     :rhymes-per-bar (/ distinct-rhyme-count (float bar-count))
+     :rhymes-per-word (/ distinct-rhyme-count (float word-count))
      :word-usage-amounts (into (sorted-map)
-                               (group-by (partial word-used-times words-sorted)
-                                         words-distinct))}))         ;make this unique
+                               (group-by (partial word-used-times sorted-words)
+                                         distinct-words))}))         ;make this unique
