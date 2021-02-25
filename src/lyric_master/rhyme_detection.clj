b87a15774e878d0ca7a8cec340ad43395859f5d7
Evan Miller <emiller.arkko@gmail.com>
2021-02-25 16:45:25 +0200

    Advance rhyme detection logic

--- src/lyric_master/rhyme_detection.clj
+++ src/lyric_master/rhyme_detection.clj
@@ -3,11 +3,11 @@
             [lyric-master.api.rhymebrain-api :as rhymebrain]))
 
 (defn api-rhyme? [w1 w2 api-get]
-  (let [get-rhyming-words #(->> %
-                                api-get
-                                (map :word))
-        w1-rhymes (get-rhyming-words w1)
-        w2-rhymes (get-rhyming-words w2)]
+  (let [get-rhymes #(->> %
+                         api-get
+                         (map :word))
+        w1-rhymes (get-rhymes w1)
+        w2-rhymes (get-rhymes w2)]
     (boolean
       (or
         ((set w1-rhymes) w2)
@@ -18,3 +18,22 @@
 
 (defn rhymebrain-rhyme? [w1 w2]
   (api-rhyme? w1 w2 rhymebrain/get-rhymes))
+
+(defn custom-rhyme-detected? [w1 w2]
+  false)
+
+(defn words-rhyme? [w1 w2]
+  (and
+    (not= w1 w2)
+    (or
+      (datamuse-rhyme? w1 w2)
+      (rhymebrain-rhyme? w1 w2)
+      (custom-rhyme-detected? w1 w2))))
+
+(defn- has-rhyme-in-coll? [word compared-words]
+  (boolean (seq
+             (filter #(words-rhyme? word %) compared-words))))
+
+(defn filter-words-that-rhyme [distinct-words]
+  (filter #(has-rhyme-in-coll? % distinct-words)
+          distinct-words))
