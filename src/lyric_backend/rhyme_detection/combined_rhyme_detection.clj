0af897bdf3fdee061d60e988de8af2f6385e0d51
Evan Miller <evan.miller@reaktor.fi>
2021-03-15 23:47:19 +0200

    Create primitive rhyme detection logic

--- src/lyric_backend/rhyme_detection/combined_rhyme_detection.clj
+++ src/lyric_backend/rhyme_detection/combined_rhyme_detection.clj
@@ -2,10 +2,21 @@
   (:require [lyric-backend.rhyme-detection.api-rhyme-detection.datamuse-api :as datamuse]
             [lyric-backend.rhyme-detection.api-rhyme-detection.rhymebrain-api :as rhymebrain]))
 
-(defn custom-rhyme-detected? [w1 w2]
-  false)
+(defn- matching-tail-n? [w1 w2 n]
+  (let [c1 (count w1)
+        c2 (count w2)]
+    (when (and (>= c1 n)
+               (>= c2 n))
+      (= (subs w1 (- c1 n) c1)
+         (subs w2 (- c2 n) c2)))))
 
-(defn any-api-found-rhyme? [w1 w2]
+(defn- matching-tail-4? [w1 w2]
+  (matching-tail-n? w1 w2 4))
+
+(defn- custom-rhyme-detected? [w1 w2]
+  (matching-tail-4? w1 w2))
+
+(defn- api-rhyme-detected? [w1 w2]
   (or (datamuse/rhyme-detected? w1 w2)
       (rhymebrain/rhyme-detected? w1 w2)))
 
@@ -15,7 +26,7 @@
     (or
       (custom-rhyme-detected? w1 w2)
       (when is-using-api
-        (any-api-found-rhyme? w1 w2)))))
+        (api-rhyme-detected? w1 w2)))))
 
 (defn- has-rhyme-in-coll? [word compared-words is-using-api]
   (boolean (seq
