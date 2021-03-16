d6d534ff02f098d23c435152627247e753bfb341
Evan Miller <evan.miller@reaktor.fi>
2021-03-16 02:52:03 +0200

    Add rhyme detection logic

--- src/lyric_backend/rhyme_detection/combined_rhyme_detection.clj
+++ src/lyric_backend/rhyme_detection/combined_rhyme_detection.clj
@@ -1,20 +1,8 @@
 (ns lyric-backend.rhyme-detection.combined-rhyme-detection
   (:require [lyric-backend.rhyme-detection.api-rhyme-detection.datamuse-api :as datamuse]
-            [lyric-backend.rhyme-detection.api-rhyme-detection.rhymebrain-api :as rhymebrain]))
+            [lyric-backend.rhyme-detection.api-rhyme-detection.rhymebrain-api :as rhymebrain]
+            [lyric-backend.rhyme-detection.simple-rhyme-detection :as simple-detection]))
 
-(defn- matching-tail-n? [w1 w2 n]
-  (let [c1 (count w1)
-        c2 (count w2)]
-    (when (and (>= c1 n)
-               (>= c2 n))
-      (= (subs w1 (- c1 n) c1)
-         (subs w2 (- c2 n) c2)))))
-
-(defn- matching-tail-4? [w1 w2]
-  (matching-tail-n? w1 w2 4))
-
-(defn- custom-rhyme-detected? [w1 w2]
-  (matching-tail-4? w1 w2))
 
 (defn- api-rhyme-detected? [w1 w2]
   (or (datamuse/rhyme-detected? w1 w2)
@@ -24,7 +12,7 @@
   (and
     (not= w1 w2)
     (or
-      (custom-rhyme-detected? w1 w2)
+      (simple-detection/rhyme-detected? w1 w2)
       (when is-using-api
         (api-rhyme-detected? w1 w2)))))
 
